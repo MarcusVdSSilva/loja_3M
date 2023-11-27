e627c44d37f3aa38b69e12c5bbbad096eb0d990f
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-11-27 20:31:08 -0300

    terminando o slider

--- App/Controller/usuario/cadastrar.php
+++ App/Controller/usuario/cadastrar.php
@@ -4,23 +4,23 @@
     //SE FOR MANDADO VIA WEB
     
     if(isset($_GET['cadastro'])){
-        if($_SERVER['REQUEST_METHOD'] == "POST"){
-            if(isset($_POST['nome']) && isset($_POST['cpf_cnpj']) && isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['telefone']) && isset($_POST['perfil'])){
+        //if($_SERVER['REQUEST_METHOD'] == "POST"){
+            //if(isset($_POST['nome']) && isset($_POST['cpf_cnpj']) && isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['telefone']) && isset($_POST['perfil'])){
                 
-                $nome = $_POST['nome'];
-                $cpf_cnpj = $_POST['cpf_cnpj'];
-                $email = $_POST['email'];
-                $senha = $_POST['senha'];
-                $telefone = $_POST['telefone'];
-                $perfil = $_POST['perfil'];
+                // $nome = $_POST['nome'];
+                // $cpf_cnpj = $_POST['cpf_cnpj'];
+                // $email = $_POST['email'];
+                // $senha = $_POST['senha'];
+                // $telefone = $_POST['telefone'];
+                // $perfil = $_POST['perfil'];
                 
                 //TESTE DE CADASTRO NO BANCO
-                /*$nome = "admin";
+                $nome = "admin";
                 $cpf_cnpj = "039.169.230-29";
                 $email = "teste@gmail.com";
                 $senha = "123";
                 $telefone = "(51) 9 96111502";
-                $perfil = "1";*/
+                $perfil = "1";
 
                 //FILTRO
                 $telefone = preg_replace("/\D/", "", "$telefone");
@@ -34,9 +34,9 @@
                 //SALVANDO NO BANCO
                 $usuario = new Usuario();
                 $cadastrar = $usuario->cadastrar($perfil,$nome,$cpf_cnpj,$email,$senha,$telefone);
-            }
+           // }
         
-        }
+        //}
         
     }else{
         //SE FOR MANDADO VIA MOBILE
