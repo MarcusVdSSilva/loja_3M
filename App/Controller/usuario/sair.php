77f88c14910c8d635eca933690e36adfbff823d9
Marcus Vinicius da Silveira Silva <contato.marcusvdss@outlook.com>
2023-12-12 19:16:18 -0300

    correções

--- App/Controller/usuario/sair.php
+++ App/Controller/usuario/sair.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Encoding");
 	session_start();
 	session_destroy();
 	header('Location: ../../View/login.php');
