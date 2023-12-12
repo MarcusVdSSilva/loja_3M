a46049072ab67eeee0f28bf9dba4437051a2f337
Kranelzin <marcelo150402@gmail.com>
2023-12-11 21:02:48 -0300

    teste update site

--- App/View/login.php
+++ App/View/login.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="../../Assets/css/login.css">
     <link rel="stylesheet" href="../../Assets/css/loginMQ.css">
-    <title>Lojas 3M - Interno</title>
+    <title>Lojas 3M (Better marcelo wins) - Interno</title>
 </head>
 <body>
     <h1>LOJAS <span class="logo">3M</span></h1>
