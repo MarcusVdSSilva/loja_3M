9f54b7934fcec62e5be75d33a8c64547ab920192
Kranelzin <marcelo150402@gmail.com>
2023-12-13 11:31:21 -0300

    ajuste cadastro venda app mobile

--- App/Controller/venda/listarCompraCliente.php
+++ App/Controller/venda/listarCompraCliente.php
@@ -4,7 +4,6 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Headers: Content-Encoding");
 require('../../Model/venda.php');
-require('../../Model/produto.php');
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){      
         if(isset($_POST['usuarioId'])){
