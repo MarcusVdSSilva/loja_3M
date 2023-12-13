b5b09bc0ff3deadf866d27792e340bf2d8a2e14d
Marcus Vinicius da Silveira Silva <contato.marcusvdss@outlook.com>
2023-12-12 21:25:53 -0300

    correções

--- App/Controller/produto/consultaId.php
+++ App/Controller/produto/consultaId.php
@@ -40,7 +40,7 @@ header("Access-Control-Allow-Headers: Content-Encoding");
                 <input type="text" name="preco" id="preco" value='.$preco.'>
 
                 <label for="imagem">Escolha uma imagem para o produto:</label>
-                <input type="file" name="imagem" id="imagem" value=../../'.$novoCaminho.' accept="image/*">
+                <input type="file" name="imagem" id="imagem" value="../../'.$novoCaminho.'" accept="image/*">
 
                 <button type="submit">Salvar Alterações</button>
 
