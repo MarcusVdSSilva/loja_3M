b51d2e12c7a3fee4778942bb3ce5d9cbaa7d212a
Marcus Vinicius da Silveira Silva <contato.marcusvdss@outlook.com>
2023-12-12 21:35:24 -0300

    correções

--- App/Controller/produto/consultaId.php
+++ App/Controller/produto/consultaId.php
@@ -22,10 +22,6 @@ header("Access-Control-Allow-Headers: Content-Encoding");
         $fotoCripto = $foto;
         $fotoCripto = base64_encode($fotoCripto);
 
-        $parteARemover = "../";
-        $novoCaminho = str_replace($parteARemover, "", $foto);
-        echo '<img src="../../'.$novoCaminho.'" alt="imagem do produto">';
-
         echo('
             <form action="../Controller/produto/editar.php?i='.$idCripto.'&f='.$fotoCripto.'" method="post" enctype="multipart/form-data">
                 
