b194c8eff93ee5a8b61e9184b39a6555e91a5de5
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-12-13 19:29:52 -0300

    editar produto finalizado

--- App/View/editarProduto.php
+++ App/View/editarProduto.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../Assets/css/editarProduto.css">
+    <link rel="stylesheet" href="../../Assets/css/editar.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <title>Lojas 3M - Administrador</title>
 </head>
@@ -17,7 +17,7 @@
     <?php require_once("header.php");?>
 
 
-    <div class="edit"><?php require_once("../Controller/produto/consultaId.php");?></div>        
+    <div class="editar botao"><?php require_once("../Controller/produto/consultaId.php");?></div>        
 
     <script src="../../Assets/js/jquery.js"></script>
     <script src="../../Assets/js/modal.js"></script>
